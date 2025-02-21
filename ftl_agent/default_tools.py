336768584dfe572a5976269c4df14764518982ed
Ben Thomasson <ben.thomasson@gmail.com>
2025-02-21 11:13:58 -0500

    Add impossible to default tools

--- ftl_agent/default_tools.py
+++ ftl_agent/default_tools.py
@@ -2,6 +2,7 @@ from smolagents.tools import Tool
 from ftl_agent.tools import get_json_schema
 from ftl_agent.local_python_executor import FinalAnswerException
 
+
 class Complete(Tool):
     name = "complete"
 
@@ -9,7 +10,7 @@ class Complete(Tool):
         self.state = state
         super().__init__(*args, **kwargs)
 
-    def forward(self, message: str):
+    def forward(self, message: str = "Task was completed"):
         """
         Mark the solution as complete.
 
@@ -21,6 +22,28 @@ class Complete(Tool):
 
     description, inputs, output_type = get_json_schema(forward)
 
+
+class Impossible(Tool):
+    name = "impossible"
+
+    def __init__(self, state, *args, **kwargs):
+        self.state = state
+        super().__init__(*args, **kwargs)
+
+    def forward(self, message: str = "Task was impossible"):
+        """
+        Mark the solution as impossible
+
+        Args:
+            message: A message explaining why the task was impossible
+        """
+
+        raise FinalAnswerException(message)
+
+    description, inputs, output_type = get_json_schema(forward)
+
+
 TOOLS = {
     "complete": Complete,
+    "impossible": Impossible,
 }
