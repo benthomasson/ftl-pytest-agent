4ebf15c85b239dc107776b266b9908429232ab21
Ben Thomasson <ben.thomasson@gmail.com>
2025-02-27 11:30:55 -0500

    Update to work with smolagents v1.9.2

--- ftl_agent/core.py
+++ ftl_agent/core.py
@@ -1,6 +1,8 @@
 from ftl_agent.agents import CodeAgent
 from smolagents import LiteLLMModel
 from ftl_agent.prompts import CODE_SYSTEM_PROMPT
+import yaml
+import importlib.resources
 
 
 def create_model(model_id, context=8192):
@@ -12,11 +14,14 @@ def create_model(model_id, context=8192):
 
 
 def run_agent(tools, model, problem_statement):
+    prompt_templates = yaml.safe_load(
+        importlib.resources.files("ftl_agent").joinpath('prompts').joinpath("code_agent.yaml").read_text()
+    )
     agent = CodeAgent(
         tools=tools,
         model=model,
         verbosity_level=4,
-        system_prompt=CODE_SYSTEM_PROMPT,
+        prompt_templates=prompt_templates,
     )
     return agent.run(problem_statement, stream=True)
 
