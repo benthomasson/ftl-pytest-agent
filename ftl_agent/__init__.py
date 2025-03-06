26a478dfe04d396209019bf9a50c22274378c100
Ben Thomasson <ben.thomasson@gmail.com>
2025-03-05 19:34:18 -0500

    build a gate with all the modules

--- ftl_agent/__init__.py
+++ ftl_agent/__init__.py
@@ -1,5 +1,8 @@
 from contextlib import contextmanager
 
+from threading import Thread
+import asyncio
+
 from smolagents.tools import Tool
 from .tools import load_tools, get_tool
 from .default_tools import TOOLS
@@ -7,10 +10,11 @@ from dataclasses import dataclass
 from faster_than_light import load_inventory
 from ftl_agent.local_python_executor import FinalAnswerException
 from faster_than_light import localhost
+from faster_than_light.gate import build_ftl_gate, use_gate
+from functools import partial
 
 
 class Tools(object):
-
     def __init__(self, tools: dict[str, Tool]):
         self.__dict__.update(tools)
 
@@ -24,10 +28,27 @@ class FTL:
 def automation(tools_files, tools, inventory, modules, **kwargs):
     tool_classes = {}
     tool_classes.update(TOOLS)
+    gate_cache = {}
+    loop = asyncio.new_event_loop()
+    thread = Thread(target=loop.run_forever, daemon=True)
+    thread.start()
+    tool_modules = tools[:]
+    if "complete" in tool_modules:
+        tool_modules.remove("complete")
     state = {
         "inventory": load_inventory(inventory),
         "modules": modules,
         "localhost": localhost,
+        "gate_cache": gate_cache,
+        "loop": loop,
+        "gate": partial(
+            use_gate,
+            *build_ftl_gate(
+                modules=tool_modules,
+                module_dirs=modules,
+                interpreter="/usr/bin/python3",
+            )
+        ),
     }
     state.update(kwargs)
     for tf in tools_files:
