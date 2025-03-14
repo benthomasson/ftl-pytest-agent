f681132e9448d9fd3edabfd049032e253f7f43af
Ben Thomasson <ben.thomasson@gmail.com>
2025-03-14 10:37:50 -0400

    Add additional info to cli2

--- ftl_pytest_agent/cli2.py
+++ ftl_pytest_agent/cli2.py
@@ -10,19 +10,26 @@ from .testgen import generate_test
 @click.command()
 @click.argument("code-file")
 @click.option("--model", "-m", default="ollama_chat/deepseek-r1:14b")
-def main(model, code_file):
+@click.option("--function", "-f", default=None)
+@click.option("--additional-info", "-a", default=None)
+def main(model, code_file, function, additional_info):
     print(code_file)
     module, fns = get_functions(code_file)
     print(module.__name__, [fn.__name__ for fn in fns])
 
     for fn in fns:
         fn_name = fn.__name__
-        fn_doc = fn.__doc__
-        tools = ['complete', fn_name]
-        prompt = f"Call {fn_name} with suitable arguments, use assert statement on the result to make sure it is correct, and then complete.  Consider the docstring for {fn_name} in your work:\n{fn_doc}\n"
-        output = f"test_{fn_name}.py"
-        explain = f"test_{fn_name}.txt"
-        generate_test(model, code_file, tools, prompt, output, explain)
+        if (function and fn_name == function) or function is None:
+            fn_doc = fn.__doc__
+            tools = ['complete', fn_name]
+            prompt = f"Call {fn_name} with suitable arguments, use assert statement on the result to make sure it is correct, and then complete.  Consider the docstring for {fn_name} in your work:\n{fn_doc}\n"
+            if additional_info:
+                with open(additional_info) as f:
+                    prompt += "\nConsider this additional info as well:\n"
+                    prompt += f.read()
+            output = f"test_{fn_name}.py"
+            explain = f"test_{fn_name}.txt"
+            generate_test(model, code_file, tools, prompt, output, explain)
 
 
 if __name__ == "__main__":
